1d8721feafe7da2dd8141f6585d87f66603a2d54
Almazq <almazbidullaev@gmail.com>
2023-11-13 00:31:31 +0600

    start

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+import Menu from './Menu.jsx'
+const Home = () =>{
+    return(
+        <div className='conteiner'>
+            <Menu />
+            <div className="working-block"></div>
+        </div>
+    )
+}
+
+// #fef0ce
+// #e6794d
+
+export default Home;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Menu.css';
+
+const Menu = () =>{
+    return(
+        <div className='menu-block'>
+            <div className="menu-block-row">
+                <h2>Menu</h2>
+                <div className="task-block">
+                    <ul>
+                        {/* link react */}
+                        <li></li>
+                    </ul>
+                </div>
+                <div className="list-block"></div>
+            </div>
+        </div>
+    )
+}
+
+// #fef0ce
+// #e6794d
+
+export default Menu;
